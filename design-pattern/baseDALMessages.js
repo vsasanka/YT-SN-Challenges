2deba5527d9d55684f524ff1f735a259f3808cc1
Sasanka Vepakomma <62672912+vsasanka@users.noreply.github.com>
2025-07-04 18:19:03 -0700

    Create baseDALMessages.js

---
+++ design-pattern/baseDALMessages.js
@@ -0,0 +1,9 @@
+var BaseDALMessages = Class.create();
+BaseDALMessages.REQUIRED_TABLE_AND_PAYLOAD = 'Table name and payload are required.';
+
+BaseDALMessages.prototype = {
+    initialize: function() {
+    },
+
+    type: 'BaseDALMessages'
+};
